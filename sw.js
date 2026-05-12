bb6751597d10a900c79858ffb57cdaaaa560375a
s1mple-911 <lazizovichasilbek@gmail.com>
2026-05-12 15:18:15 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // AROS Market Service Worker
 // Versiyani har push'da o'zgartiring (cache reset)
-const CACHE_VERSION = 'aros-v9';
+const CACHE_VERSION = 'aros-v10';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 
 // Sahifalar (cache qilinadi)
